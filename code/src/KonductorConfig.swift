0f74ada953716b40f12b88bb0041bf9a3b94ff9f
Kevin Lind <lind@adobe.com>
2020-04-14 16:59:44 -0700

    [amsdk-9554] KonductorConfig extends Codable to simplify struct and reduce code.

--- code/src/KonductorConfig.swift
+++ code/src/KonductorConfig.swift
@@ -18,33 +18,12 @@ import Foundation
 
 /// Konductor configuration metadata.
 /// Is contained within the `RequestMetadata` request property.
-struct KonductorConfig {
+struct KonductorConfig : Codable {
     /// The IMS Org ID. Typically this is the Experience Platform Org ID.
     var imsOrgId: String?
     
     /// Configure Konductor to provide the response fragments in a streaming fashion.
     var streaming: Streaming?
-    
-    enum CodingKeys: String, CodingKey {
-        case imsOrgId = "imsOrgId"
-        case streaming = "streaming"
-    }
-}
-
-extension KonductorConfig : Encodable {
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        if let unwrapped = imsOrgId { try container.encode(unwrapped, forKey: .imsOrgId)}
-        if let unwrapped = streaming { try container.encode(unwrapped, forKey: .streaming)}
-    }
-}
-
-extension KonductorConfig : Decodable {
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        imsOrgId = try? container.decode(String.self, forKey: .imsOrgId)
-        streaming = try? container.decode(Streaming.self, forKey: .streaming)
-    }
 }
 
 /// Konductor configuration metadata to provide response fragments in a streaming fashion (HTTP 1.1/chunked, IETF RFC 7464).
