e9dbfb03c7ecd995706c5c0d984195390595e5af
Kevin Lind <lind@adobe.com>
2020-05-22 12:59:59 -0700

    [amsdk-9772] Add simple demo app which pulls the ACPExperiencePlatform from a Podfile.

---
+++ demo/AEPDemoApp/AEPDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AEPDemoApp
+//
+//  Created by lind on 5/21/20.
+//  Copyright © 2020 Adobe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
