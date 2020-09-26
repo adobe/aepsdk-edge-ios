701d4479cd2d7d2e988b8edea795be4ab665631d
Emilia Dobrin <dobrin@adobe.com>
2020-09-25 17:48:44 -0700

    [DEV] Update swiftlint config path

--- code/src/EdgeNetworkHandlers/ExperiencePlatformNetworkService.swift
+++ code/src/EdgeNetworkHandlers/ExperiencePlatformNetworkService.swift
@@ -76,7 +76,8 @@ class ExperiencePlatformNetworkService {
     ///   - responseCallback: `ResponseCallback` to be invoked once the server response is received
     ///   - retryTimes: number of retries required for this request in case the connection failed or a `recoverableNetworkErrorCodes` was encountered
     func doRequest(url: URL, requestBody: EdgeRequest, requestHeaders: [String: String]? = [:], responseCallback: ResponseCallback, retryTimes: UInt = 0) {
-        // AMSDK-8909 check if this network request fails and needs a retry. The retry will happen right away, repeatedly (if needed) for maximim retryTimes or until the request is accepted by the server.
+        // AMSDK-8909 check if this network request fails and needs a retry. The retry will happen right away, repeatedly (if needed)
+        // for maximim retryTimes or until the request is accepted by the server.
         // To be reconsidered when implementing AMSDK-9829 when we may not need the max retries limit anymore.
 
         var shouldRetry = self.doRequest(url: url, requestBody: requestBody, requestHeaders: requestHeaders, responseCallback: responseCallback)
