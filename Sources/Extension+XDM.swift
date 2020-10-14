66a0c36aec753d1548be6e178c2cfb22ec3b42d8
Nick Porter <nporter@adobe.com>
2020-10-14 13:24:36 -0600

    Add doc comments

--- Sources/Extension+XDM.swift
+++ Sources/Extension+XDM.swift
@@ -16,6 +16,9 @@ import AEPCore
 
 /// Provides functionality for setting and reading XDM shared state
 public extension Extension {
+    
+    /// Appends `data` to the XDM shared state, if any of the keys in `data` map to existing keys in the XDM shared state, those values will be updated with the values in `data`.
+    /// - Parameter data: Data to be appended to XDM shared state
     func createXDMSharedState(data: [String: Any]) {
         let existingXDMSharedState = getXDMSharedState()?.value ?? [:]
         // add the new shared state on-top of existing shared state
@@ -23,6 +26,8 @@ public extension Extension {
         createSharedState(data: newXDMSharedState, event: nil) // always version at latest, shared state is published to "com.adobe.edge"
     }
     
+    /// Reads and returns the XDM shared state
+    /// - Returns: The XDM shared state
     func getXDMSharedState() -> SharedStateResult? {
         return getSharedState(extensionName: Constants.EXTENSION_NAME, event: nil)
     }
