bb2cb2ecd32139f749de71922df6b4569ab5acae
Nick Porter <nporter@adobe.com>
2021-01-08 15:29:14 -0700

    Add clarifying comment about not supporting http-date

--- Sources/EdgeNetworkHandlers/EdgeNetworkService.swift
+++ Sources/EdgeNetworkHandlers/EdgeNetworkService.swift
@@ -124,6 +124,7 @@ class EdgeNetworkService {
                     Log.debug(label: self.LOG_TAG, "doRequest - Connection to Experience Edge returned recoverable error code \(responseCode)")
                     let retryHeader = connection.responseHttpHeader(forKey: EdgeConstants.NetworkKeys.HEADER_KEY_RETRY_AFTER)
                     var retryInterval = EdgeConstants.NetworkKeys.RETRY_INTERVAL
+                    // Do not currently support HTTP-date only parsing Ints for now. Konductor will only send back Retry-After as Ints.
                     if let retryHeader = retryHeader, let retryAfterInterval = TimeInterval(retryHeader) {
                         retryInterval = retryAfterInterval
                     }
