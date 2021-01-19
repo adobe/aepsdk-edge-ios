03b680594ed2f48eecde3d202fdc76b53e8e63de
Nick Porter <43650450+nporter-adbe@users.noreply.github.com>
2021-01-19 12:07:14 -0700

    Update ObjC API parameter names

--- Sources/Edge+PublicAPI.swift
+++ Sources/Edge+PublicAPI.swift
@@ -22,7 +22,7 @@ public extension Edge {
     ///   - experienceEvent: Event to be sent to Adobe Experience Edge
     ///   - completion: Optional completion handler to be invoked when the request is complete, returning the associated response handles
     ///                 received from the Adobe Experience Edge. It may be invoked on a different thread.
-    @objc(sendEvent:experienceEvent:)
+    @objc(sendExperienceEvent:completion:)
     static func sendEvent(experienceEvent: ExperienceEvent, _ completion: (([EdgeEventHandle]) -> Void)? = nil) {
         guard let xdmData = experienceEvent.xdm, !xdmData.isEmpty, let eventData = experienceEvent.asDictionary() else {
             Log.debug(label: LOG_TAG, "Failed to dispatch the experience event because the XDM data was nil/empty.")
