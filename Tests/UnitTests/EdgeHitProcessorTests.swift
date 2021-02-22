da550a946781f0bd4fd4a374ecdcdbfc58fc70b0
Nick Porter <nporter@adobe.com>
2021-02-21 17:46:58 -0700

    Use "test-ecid"

--- Tests/UnitTests/EdgeHitProcessorTests.swift
+++ Tests/UnitTests/EdgeHitProcessorTests.swift
@@ -67,7 +67,7 @@ class EdgeHitProcessorTests: XCTestCase {
                     "ECID" : [
                       {
                         "authenticationState" : "ambiguous",
-                        "id" : "test-mcid",
+                        "id" : "test-ecid",
                         "primary" : false
                       }
                     ]
