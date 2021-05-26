55b6b1613ace870c96f1a7dd6abb9ad89a8b59e6
Emilia Dobrin <dobrin@adobe.com>
2021-05-26 13:23:58 -0700

    Bump version to 1.1.1

--- Sources/EdgeConstants.swift
+++ Sources/EdgeConstants.swift
@@ -16,7 +16,7 @@ import Foundation
 enum EdgeConstants {
 
     static let EXTENSION_NAME = "com.adobe.edge"
-    static let EXTENSION_VERSION = "1.1.0"
+    static let EXTENSION_VERSION = "1.1.1"
     static let FRIENDLY_NAME = "AEPEdge"
     static let LOG_TAG = FRIENDLY_NAME
 
