207ce85bf02a8618218b57d3b7d56c85f8be2d57
Kevin Lind <lind@adobe.com>
2021-12-01 21:38:02 -0800

    Clean up ImplementationDetails code by extracting logic to parse wrapper name in separate function.

--- Sources/ImplementationDetails.swift
+++ Sources/ImplementationDetails.swift
@@ -21,7 +21,7 @@ enum ImplementationDetails {
     /// If the Mobile Core "wrapper" exists but the "type" cannot be parsed, then "unknown" is used for the wrapper type.
     /// If the "wrapper" does not exist in `hubState`, or the found wrapper type is not supported, then the default `WrapperType.None` is used.
     ///
-    /// - Parameter hubState: the shared state of the `EventHub`
+    /// - Parameter hubState: `EventHub` shared state`
     /// - Returns: the implementation details for current session, or nil if `hubState` is nil or empty
     static func from(_ hubState: [String: Any]?) -> [String: Any]? {
         guard let hubState = hubState, !hubState.isEmpty else {
@@ -31,26 +31,34 @@ enum ImplementationDetails {
         // if core version is not found set to "unknown"
         let coreVersion: String = hubState[EdgeConstants.SharedState.Hub.VERSION] as? String ?? EdgeConstants.JsonValues.ImplementationDetails.UNKNOWN
 
-        var wrapperName: String = ""
-
-        if hubState[EdgeConstants.SharedState.Hub.WRAPPER] != nil {
-            if let wrapperType = (hubState[EdgeConstants.SharedState.Hub.WRAPPER] as? [String: Any])?[EdgeConstants.SharedState.Hub.TYPE] as? String {
-                switch wrapperType {
-                case "R": // React Native
-                    wrapperName = "/\(EdgeConstants.JsonValues.ImplementationDetails.WRAPPER_REACT_NATIVE)" // note forward slash
-                default:
-                    wrapperName = "" // unsupported wrapper type defaults to none
-                }
-            } else {
-                // "wrapper" entry exists but "type" not found. Unexpected, set to "unknown"
-                wrapperName = "/\(EdgeConstants.JsonValues.ImplementationDetails.UNKNOWN)"
-            }
-        }
+        let wrapperName: String = getWrapperName(from: hubState)
 
         return [
             EdgeConstants.JsonKeys.ImplementationDetails.VERSION: "\(coreVersion)+\(EdgeConstants.EXTENSION_VERSION)",
             EdgeConstants.JsonKeys.ImplementationDetails.NAME: "\(EdgeConstants.JsonValues.ImplementationDetails.BASE_NAMESPACE)\(wrapperName)",
             EdgeConstants.JsonKeys.ImplementationDetails.ENVIRONMENT: EdgeConstants.JsonValues.ImplementationDetails.ENVIRONMENT_APP
         ]
+    }
+
+    /// Get the wrapper named used for the Implementation Details namespace from the Event Hub shared state.
+    ///
+    /// - Parameter hubState: `EventHub` shared state
+    /// - Returns: wrapper type name used in the Implementation Details namespace, or an empty string if no wrapper is set
+    private static func getWrapperName(from hubState: [String: Any]) -> String {
+        if hubState[EdgeConstants.SharedState.Hub.WRAPPER] != nil {
+            if let wrapperType = (hubState[EdgeConstants.SharedState.Hub.WRAPPER] as? [String: Any])?[EdgeConstants.SharedState.Hub.TYPE] as? String {
+                switch wrapperType {
+                case "R": // React Native
+                    return "/\(EdgeConstants.JsonValues.ImplementationDetails.WRAPPER_REACT_NATIVE)" // note forward slash
+                default:
+                    return "" // unsupported wrapper type defaults to none
+                }
+            } else {
+                // "wrapper" entry exists but "type" not found. Unexpected, set to "unknown"
+                return "/\(EdgeConstants.JsonValues.ImplementationDetails.UNKNOWN)" // note forward slash
+            }
+        }
+
+        return ""
     }
 }
