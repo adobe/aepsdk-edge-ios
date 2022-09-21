a74015782c5e9cc634b628b20ef37c01119d5091
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2022-09-21 13:25:28 -0700

    Update button functions to match ios app

--- Documentation/Tutorials/EdgeTutorialAppStart/app/src/main/java/com/adobe/marketing/mobile/tutorial/FirstFragment.java
+++ Documentation/Tutorials/EdgeTutorialAppStart/app/src/main/java/com/adobe/marketing/mobile/tutorial/FirstFragment.java
@@ -20,6 +20,7 @@ import com.adobe.marketing.mobile.xdm.Commerce;
 import com.adobe.marketing.mobile.xdm.MobileSDKCommerceSchema;
 import com.adobe.marketing.mobile.xdm.Order;
 import com.adobe.marketing.mobile.xdm.ProductListAdds;
+import com.adobe.marketing.mobile.xdm.ProductListItemsItem;
 import com.adobe.marketing.mobile.xdm.Purchases;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -48,14 +49,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
-            }
-        });
-
+        // Product add event - button action
         binding.productAddEventButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,24 +62,46 @@ public class FirstFragment extends Fragment {
                 eventData.put("listExample", valuesList);
 
                 // Create XDM data with Commerce data for purchases action
-                MobileSDKCommerceSchema xdmData = new MobileSDKCommerceSchema();
-                Order order = new Order();
-                order.setCurrencyCode("RON");
-                order.setPriceTotal(20);
-                Purchases purchases = new Purchases();
-                purchases.setValue(1);
-                ProductListAdds products = new ProductListAdds();
-                products.setValue(21);
+//                MobileSDKCommerceSchema xdmData = new MobileSDKCommerceSchema();
+//                Order order = new Order();
+//                order.setCurrencyCode("RON");
+//                order.setPriceTotal(20);
+//                Purchases purchases = new Purchases();
+//                purchases.setValue(1);
+//                ProductListAdds products = new ProductListAdds();
+//                products.setValue(21);
+//                Commerce commerce = new Commerce();
+//                commerce.setOrder(order);
+//                commerce.setProductListAdds(products);
+//                commerce.setPurchases(purchases);
+//                xdmData.setEventType("commerce.purchases");
+//                xdmData.setCommerce(commerce);
+
+
+
+                ProductListItemsItem product = new ProductListItemsItem();
+                product.setName("wide_brim_sunhat");
+                product.setPriceTotal(50);
+                product.setSKU("12345");
+                product.setQuantity(1);
+                product.setCurrencyCode("USD");
+
+                List<ProductListItemsItem> productListItems = new ArrayList<>();
+                productListItems.add(product);
+
+                ProductListAdds productAdd = new ProductListAdds();
+                productAdd.setValue(1);
+
                 Commerce commerce = new Commerce();
-                commerce.setOrder(order);
-                commerce.setProductListAdds(products);
-                commerce.setPurchases(purchases);
-                xdmData.setEventType("commerce.purchases");
+                commerce.setProductListAdds(productAdd);
+
+                MobileSDKCommerceSchema xdmData = new MobileSDKCommerceSchema();
+                xdmData.setEventType("commerce.productListAdds");
                 xdmData.setCommerce(commerce);
+                xdmData.setProductListItems(productListItems);
 
                 ExperienceEvent event = new ExperienceEvent.Builder()
                         .setXdmSchema(xdmData)
-                        .setData(eventData)
                         .build();
                 Log.d(LOG_TAG, "Sending event");
                 Edge.sendEvent(event, new EdgeCallback() {
@@ -112,6 +128,63 @@ public class FirstFragment extends Fragment {
                         });
                     }
                 });
+            }
+        });
+
+        // Product view event - button action
+        binding.productViewEventButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Construct commerce data body
+                Map<String, Object> productListViewsData = new HashMap<>();
+                productListViewsData.put("value", 1);
+
+                Map<String, Object> productListViews = new HashMap<>();
+                productListViews.put("productListViews", productListViewsData);
+
+                // Construct productListItems data body
+                Map<String, Object> productListItem = new HashMap<>();
+                productListItem.put("name", "wide_brim_sunhat");
+                productListItem.put("SKU", "12345");
+
+                List<Object> productList = new ArrayList<>();
+                productList.add(productListItem);
+
+                // Put together final XDM event data payload
+                Map<String, Object> eventData = new HashMap<>();
+                eventData.put("eventType", "commerce.productViews");
+                eventData.put("commerce", productListViews);
+                eventData.put("productListItems", productList);
+
+                ExperienceEvent event = new ExperienceEvent.Builder()
+                        .setXdmSchema(eventData)
+                        .build();
+                Log.d(LOG_TAG, "Sending event");
+                Edge.sendEvent(event, new EdgeCallback() {
+
+                    @Override
+                    public void onComplete(final List<EdgeEventHandle> handles) {
+//                        MobileCore.log(LoggingMode.VERBOSE, LOG_TAG, "Data received in the callback, updating UI");
+                        Log.d(LOG_TAG, "Edge event callback called");
+                        if (handles == null) {
+                            return;
+                        }
+
+                        view.post(new Runnable() {
+                            @Override
+                            public void run() {
+//                                if (textViewGetData != null) {
+//
+//                                    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//                                    String json = gson.toJson(handles);
+//                                    Log.d(LOG_TAG, String.format("Received Edge event handle are : %s", json));
+///
+//                                }
+                            }
+                        });
+                    }
+                });
+
             }
         });
     }
--- Documentation/Tutorials/EdgeTutorialAppStart/app/src/main/java/com/adobe/marketing/mobile/tutorial/MainActivity.java
+++ Documentation/Tutorials/EdgeTutorialAppStart/app/src/main/java/com/adobe/marketing/mobile/tutorial/MainActivity.java
@@ -70,13 +70,13 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
