8176e699c6ca9413958842e6d079e8ed63236d3e
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2023-04-03 16:30:12 -0700

    Split test enums into separate files with shared util method

--- Tests/UpstreamIntegrationTests/util/EdgeLocationHint.swift
+++ Tests/UpstreamIntegrationTests/util/EdgeLocationHint.swift
@@ -13,38 +13,6 @@
 
 import Foundation
 
-fileprivate func extractEnvironmentVariable<T: RawRepresentable>(keyName: String, enum: T.Type) -> T? where T.RawValue == String {
-    guard let environmentString = ProcessInfo.processInfo.environment[keyName] else {
-        print("Unable to find valid \(keyName) value (raw value: \(String(describing: ProcessInfo.processInfo.environment[keyName]))).")
-        return nil
-    }
-    
-    guard let enumCase = T(rawValue: environmentString) else {
-        print("Unable to create valid enum case of type \(T.Type.self) from environment variable value: \(environmentString)")
-        return nil
-    }
-    
-    return enumCase
-}
-
-/// Edge Network environment levels that correspond to actual deployment environment levels
-enum EdgeEnvironment: String {
-    /// Production
-    case prod
-    /// Pre-production - aka: staging
-    case preProd = "pre-prod"
-    /// Integration - aka: development
-    case int
-    
-    /// Initializer that gets the value from the environment variable `EDGE_ENVIRONMENT` and creates an `EdgeEnvironment` instance.
-    init?() {
-        guard let edgeEnvironment = extractEnvironmentVariable(keyName: "EDGE_ENVIRONMENT", enum: EdgeEnvironment.self) else {
-            return nil
-        }
-        self = edgeEnvironment
-    }
-}
-
 /// All location hint values available for the Edge Network extension
 enum EdgeLocationHint: String {
     /// Oregon, USA
