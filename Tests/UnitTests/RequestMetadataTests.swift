8189f9ad042f0fc2f2e61361334ff8d9316f2740
timkimadobe <95260439+timkimadobe@users.noreply.github.com>
2023-11-22 17:12:22 -0800

    Refactor RequestMetadataTests to use JSON comparison tool

--- Tests/UnitTests/RequestMetadataTests.swift
+++ Tests/UnitTests/RequestMetadataTests.swift
@@ -15,91 +15,118 @@ import AEPServices
 import AEPTestUtils
 import XCTest
 
-class RequestMetadataTests: XCTestCase {
+class RequestMetadataTests: XCTestCase, AnyCodableAsserts {
+    private let encoder = JSONEncoder()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         continueAfterFailure = false // fail so nil checks stop execution
+        encoder.outputFormatting = [.prettyPrinted]
+        encoder.dateEncodingStrategy = .iso8601
     }
 
     // MARK: encoder tests
 
     func testEncode_noParameters() {
         let metadata = RequestMetadata(konductorConfig: nil, sdkConfig: nil, configOverrides: nil, state: nil)
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        encoder.dateEncodingStrategy = .iso8601
-
-        let data = try? encoder.encode(metadata)
-        let actualResult = asFlattenDictionary(data: data)
-        let expectedResult: [String: Any] = [:]
-        assertEqual(expectedResult, actualResult)
+        
+        guard let data = try? encoder.encode(metadata), let metadataString = String(data: data, encoding: .utf8) else {
+            XCTFail("Unable to encode/decode RequestMetadata: \(metadata)")
+            return
+        }
+        
+        let expectedJSON = "{}"
+        assertEqual(expected: getAnyCodable(expectedJSON)!, actual: getAnyCodable(metadataString))
     }
 
     func testEncode_paramKonductorConfig() {
         let metadata = RequestMetadata(konductorConfig: KonductorConfig(streaming: nil), sdkConfig: nil, configOverrides: nil, state: nil)
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        encoder.dateEncodingStrategy = .iso8601
-
-        let data = try? encoder.encode(metadata)
-        let actualResult = asFlattenDictionary(data: data)
-        let expectedResult: [String: Any] = ["konductorConfig": "isEmpty"]
-        assertEqual(expectedResult, actualResult)
+        
+        guard let data = try? encoder.encode(metadata), let metadataString = String(data: data, encoding: .utf8) else {
+            XCTFail("Unable to encode/decode RequestMetadata: \(metadata)")
+            return
+        }
+        
+        let expectedJSON = #"""
+        {
+          "konductorConfig": {}
+        }
+        """#
+        assertEqual(expected: getAnyCodable(expectedJSON)!, actual: getAnyCodable(metadataString))
     }
 
     func testEncode_paramStateMetadata() {
         let payload = StorePayload(key: "key", value: "value", maxAge: 3600)
         let metadata = RequestMetadata(konductorConfig: nil, sdkConfig: nil, configOverrides: nil, state: StateMetadata(payload: [payload]))
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        encoder.dateEncodingStrategy = .iso8601
-
-        let data = try? encoder.encode(metadata)
-        let actualResult = asFlattenDictionary(data: data)
-        let expectedResult: [String: Any] =
-            ["state.entries[0].key": "key",
-             "state.entries[0].maxAge": 3600,
-             "state.entries[0].value": "value"]
-        assertEqual(expectedResult, actualResult)
+        
+        guard let data = try? encoder.encode(metadata), let metadataString = String(data: data, encoding: .utf8) else {
+            XCTFail("Unable to encode/decode RequestMetadata: \(metadata)")
+            return
+        }
+        
+        let expectedJSON = #"""
+        {
+          "state": {
+            "entries": [
+              {
+                "key": "key",
+                "maxAge": 3600,
+                "value": "value"
+              }
+            ]
+          }
+        }
+        """#
+        assertEqual(expected: getAnyCodable(expectedJSON)!, actual: getAnyCodable(metadataString))
     }
 
     func testEncode_paramKonductorConfig_paramStateMetadata() {
         let payload = StorePayload(key: "key", value: "value", maxAge: 3600)
         let metadata = RequestMetadata(konductorConfig: KonductorConfig(streaming: nil), sdkConfig: nil, configOverrides: nil,
                                        state: StateMetadata(payload: [payload]))
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        encoder.dateEncodingStrategy = .iso8601
-
-        let data = try? encoder.encode(metadata)
-        let actualResult = asFlattenDictionary(data: data)
-        let expectedResult: [String: Any] =
-            ["state.entries[0].key": "key",
-             "state.entries[0].maxAge": 3600,
-             "state.entries[0].value": "value",
-             "konductorConfig": "isEmpty"]
-        assertEqual(expectedResult, actualResult)
+        
+        guard let data = try? encoder.encode(metadata), let metadataString = String(data: data, encoding: .utf8) else {
+            XCTFail("Unable to encode/decode RequestMetadata: \(metadata)")
+            return
+        }
+        
+        let expectedJSON = #"""
+        {
+          "konductorConfig": {},
+          "state": {
+            "entries": [
+              {
+                "key": "key",
+                "maxAge": 3600,
+                "value": "value"
+              }
+            ]
+          }
+        }
+        """#
+        assertEqual(expected: getAnyCodable(expectedJSON)!, actual: getAnyCodable(metadataString))
     }
 
     func testEncode_paramSDKConfig_originalDatastreamIdMetadata() {
         let payload = StorePayload(key: "key", value: "value", maxAge: 3600)
         let metadata = RequestMetadata(konductorConfig: KonductorConfig(streaming: nil), sdkConfig: SDKConfig(datastream: Datastream(original: "OriginalDatastreamID")), configOverrides: nil, state: nil)
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        encoder.dateEncodingStrategy = .iso8601
-
-        let data = try? encoder.encode(metadata)
-        let actualResult = asFlattenDictionary(data: data)
-        let expectedResult: [String: Any] =
-            ["sdkConfig.datastream.original": "OriginalDatastreamID",
-             "konductorConfig": "isEmpty"]
-        assertEqual(expectedResult, actualResult)
+        
+        guard let data = try? encoder.encode(metadata), let metadataString = String(data: data, encoding: .utf8) else {
+            XCTFail("Unable to encode/decode RequestMetadata: \(metadata)")
+            return
+        }
+        
+        let expectedJSON = #"""
+        {
+          "konductorConfig": {},
+          "sdkConfig": {
+            "datastream": {
+              "original": "OriginalDatastreamID"
+            }
+          }
+        }
+        """#
+        assertEqual(expected: getAnyCodable(expectedJSON)!, actual: getAnyCodable(metadataString))
     }
 
     func testEncode_paramConfigOverrides_originalDatastreamConfigOverrideMetadata() {
@@ -131,23 +158,42 @@ class RequestMetadataTests: XCTestCase {
 
         let metadata = RequestMetadata(konductorConfig: KonductorConfig(streaming: nil),
                                        sdkConfig: nil, configOverrides: AnyCodable.from(dictionary: configOverrides), state: nil)
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.prettyPrinted]
-        encoder.dateEncodingStrategy = .iso8601
-
-        let data = try? encoder.encode(metadata)
-        let actualResult = asFlattenDictionary(data: data)
-        let expectedResult: [String: Any] =
-            ["configOverrides.com_adobe_experience_platform.datasets.event.datasetId": "testEventDatasetIdOverride",
-             "configOverrides.com_adobe_experience_platform.datasets.profile.datasetId": "testProfileDatasetIdOverride",
-             "configOverrides.com_adobe_analytics.reportSuites[0]": "rsid1",
-             "configOverrides.com_adobe_analytics.reportSuites[1]": "rsid2",
-             "configOverrides.com_adobe_analytics.reportSuites[2]": "rsid3",
-             "configOverrides.com_adobe_identity.idSyncContainerId": "1234567",
-             "configOverrides.com_adobe_target.propertyToken": "testPropertyToken",
-
-             "konductorConfig": "isEmpty"]
-        assertEqual(expectedResult, actualResult)
+        
+        guard let data = try? encoder.encode(metadata), let metadataString = String(data: data, encoding: .utf8) else {
+            XCTFail("Unable to encode/decode RequestMetadata: \(metadata)")
+            return
+        }
+        
+        let expectedJSON = #"""
+        {
+          "configOverrides": {
+            "com_adobe_analytics": {
+              "reportSuites": [
+                "rsid1",
+                "rsid2",
+                "rsid3"
+              ]
+            },
+            "com_adobe_experience_platform": {
+              "datasets": {
+                "event": {
+                  "datasetId": "testEventDatasetIdOverride"
+                },
+                "profile": {
+                  "datasetId": "testProfileDatasetIdOverride"
+                }
+              }
+            },
+            "com_adobe_identity": {
+              "idSyncContainerId": "1234567"
+            },
+            "com_adobe_target": {
+              "propertyToken": "testPropertyToken"
+            }
+          },
+          "konductorConfig": {}
+        }
+        """#
+        assertEqual(expected: getAnyCodable(expectedJSON)!, actual: getAnyCodable(metadataString))
     }
 }
